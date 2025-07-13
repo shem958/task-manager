0182f8c213818d8464620c36e94bfe77c9f25263
shem958 <shemgikunda37@gmail.com>
2025-07-14 00:06:16 +0300

    Refactor routes to include authentication and task resource management

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Auth::routes();
+
+Route::middleware('auth')->group(function () {
+    Route::resource('tasks', TaskController::class);
 });
